ac8110e1037977adc0796dba1f456ad1b3754a7b
Alessandro Canova <alessandrocanova14@gmail.com>
2020-09-25 23:22:33 +0200

    Add router

--- src/App.js
+++ src/App.js
@@ -1,31 +1,55 @@
 import React, { Component } from 'react';
+import {
+  HashRouter,
+  Route,
+  Switch
+} from 'react-router-dom';
 import './App.css';
+
+// Components
 import SearchForm from './Components/SearchForm';
 import Nav from './Components/Nav';
 import PhotoContainer from './Components/PhotoContainer';
+import NotFound from './Components/NotFound';
 //import Photo from './Components/Photo';
 
 
 class App extends Component  {
+
+  constructor() {
+
+    super();
+
+    this.state = {
+      photos: [],
+      loading: true
+    }
+
+  } 
 
   
   render () {
 
     return (
 
-      <div className="container">
+      <HashRouter>
+      
+        <Route render={ () => <SearchForm />} />
+        <Route render={ () => <Nav />} />
 
-        <SearchForm />
-
-        <Nav />
-        <PhotoContainer />
-
-      </div>
+        <Switch>
+          <Route path='/search/....' render={ () => <PhotoContainer /> } />
+          <Route path='/' render={ () => <NotFound />} />
+        </Switch>
+        
+      </HashRouter>
 
     );
 
   }
 
 }
+
+
 
 export default App;
