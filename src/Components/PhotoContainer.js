ee7331d8061b8a8debd3fb4dc4240691b70d06e1
Alessandro Canova <alessandrocanova14@gmail.com>
2020-09-29 21:56:39 +0200

    Updated with its data

--- src/Components/PhotoContainer.js
+++ src/Components/PhotoContainer.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
 import Photo from './Photo';
 import NotFound from './NotFound';
+import { withRouter } from "react-router"; //withRouter will pass updated match and history props to the wrapped component whenever it renders.
+
 
 class PhotoContainer extends Component {
 
+
     render() {
+
+        // store the data in a costant
+        const results = this.props.photos
+        let photos;
+
+        if(results.length > 0) {
+
+            // return a Photo component (<Photo/>) from each objecy of the array.
+            photos = results.map( photo => { <Photo photo={photo} key={photo.id} /> })
+
+        } else {
+
+            photos = <NotFound />
+
+        }
 
         return (
 
             <div class="photo-container">
                 <h2>Results</h2>
                     <ul>
-                        <Photo />
-                        <NotFound />
+                        {photos}
                     </ul>
             </div>
 
@@ -22,4 +39,4 @@ class PhotoContainer extends Component {
 
 }
 
-export default PhotoContainer;
+export default withRouter(PhotoContainer);
