44c5593b1b1f83b2283d3e16f59287099ae5c2eb
Alessandro Canova <alessandrocanova14@gmail.com>
2020-09-29 20:49:33 +0200

    Fetched data

--- src/App.js
+++ src/App.js
@@ -5,13 +5,16 @@ import {
   Switch
 } from 'react-router-dom';
 import './App.css';
+import axios from 'axios';
+import api_key from './config';
 
 // Components
 import SearchForm from './Components/SearchForm';
 import Nav from './Components/Nav';
 import PhotoContainer from './Components/PhotoContainer';
 import NotFound from './Components/NotFound';
-//import Photo from './Components/Photo';
+
+
 
 
 class App extends Component  {
@@ -22,10 +25,33 @@ class App extends Component  {
 
     this.state = {
       photos: [],
+      query: '',
       loading: true
     }
 
   } 
+
+  /* Fetching Data with Axios */
+  componentDidMount () {
+    this.performSearch();
+  }
+
+  performSearch = query => {
+
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${api_key}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
+      .then(response => {
+        this.setState({
+          photos: response.photos.photo,
+          query: query,
+          loading: false
+        });
+      })
+      .catch(error => {
+        console.log('Error fetching and parsing data', error);
+      });
+
+  }
+  
 
   
   render () {
@@ -33,12 +59,16 @@ class App extends Component  {
     return (
 
       <HashRouter>
-      
+
         <Route render={ () => <SearchForm />} />
         <Route render={ () => <Nav />} />
 
         <Switch>
-          <Route path='/search/....' render={ () => <PhotoContainer /> } />
+          <Route path='/search/:query' render={ () => <PhotoContainer 
+                                                          photos={this.state.photos}
+                                                          query={this.state.query}
+                                                          loading={this.state.loading}
+                                                      /> } />
           <Route path='/' render={ () => <NotFound />} />
         </Switch>
         
