045ee706daa418c161e56ce2b77b1b252bfb24c8
alecanova <alessandrocanova14@gmail.com>
2020-10-11 20:20:27 +0200

    Styling

--- src/Components/ErrorPage.js
+++ src/Components/ErrorPage.js
@@ -5,7 +5,7 @@ const ErrorPage = () => (
     <div className="not-found">
         <h2>404: oh noes, there's nothing in here!</h2>
         <p>The page you are trying to reach does not exist.</p>
-        <Link to="/">Home page</Link>
+        <Link to="/"><p className='home'>Home page</p></Link>
     </div>
 )
 
